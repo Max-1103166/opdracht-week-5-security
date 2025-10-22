eb37d9fbb251da5d86353368ec5f4b0073353593
Max-1103166 <1103166@hr.nl>
2025-10-22 14:17:51 +0200

    added PBKDF2 to fernet encryption updated the styling and added bootstrap

--- app/models/encryption.py
+++ app/models/encryption.py
@@ -1,11 +1,26 @@
+import os
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
 
 class Encryption:
     def __init__(self):
         pass
 
     def encrypt(self, user_id, message, receiver_id):
-        key = Fernet.generate_key()
+        receiver_password = "dit is een geheim wachtwoord"
+
+        salt = os.urandom(16)
+
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=1_200_000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(receiver_password.encode()))
         fernet = Fernet(key)
         message = fernet.encrypt(message.encode())
-        return [user_id,message.decode(),receiver_id]
+
+        return message.decode()
